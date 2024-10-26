ef5db8e079dadddc00929d6bd7e9a15c842bc1d7
Manu577228 <bnmanubharadwaj@gmail.com>
2024-10-26 14:39:26 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
